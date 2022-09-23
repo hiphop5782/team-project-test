d603d3abfe84581a6d3531d070ca33aa251aaf19
hiphop5782 <zdlsz@hanmail.net>
2022-09-23 16:07:51 +0900

    작업 업로드

---
+++ demo/src/main/java/com/hacademy/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.hacademy.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {
+		return "Welcome!";
+	}
+	
+}
